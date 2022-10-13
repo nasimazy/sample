020b508d076250ef1a2d82051e9d3d201af1d28c
Pumpy <ptnasimazy@gmail.com>
2022-10-13 13:02:37 +0530

    commit

---
+++ bin/abstract.dart
@@ -0,0 +1,23 @@
+abstract class A {
+  // abstract class
+  int a = 100;
+
+  void show() {
+    print('show func of A');
+  }
+
+  void display(); //abstract func
+}
+class B extends A{
+  @override
+  void display() {
+    print('overridden display function');
+  }
+  }
+  void main() {
+    B obj = B();
+    obj.show();
+    obj.display();
+    print(obj.a);
+  }
+}
